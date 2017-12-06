c8803775190ee5a058730741fde6ba2600ff9487
G. Reiter <guntbert@gmx.at>
2017-12-06 17:39:19 +0100

    Thats the solution for both problems

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path="/home/re/develop/AdventOfCode-2017/Day1/inputstring";
+            string inputstring=System.IO.File.ReadAllText(path);
+            char[]captcha=inputstring.ToCharArray();
+            int len = captcha.Length;
+            int sum=0;
+            //part 1
+            int distance = 1;
+            //part 2
+            distance = len/2;
+            for (int i = 0; i < captcha.Length; i++)
+            {
+                int j= (i+distance)%len;
+                if (captcha[i]==captcha[j])
+                {
+                    int value = (int) char.GetNumericValue(captcha[i]);
+sum +=value;
+                }
+            }
+// 1393
+
+
+            
+            Console.WriteLine(value: $"Summe = {sum}");
+        }
+    }
+}
